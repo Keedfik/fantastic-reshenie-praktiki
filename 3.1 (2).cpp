457849df48a03577a724eae47c011cc316103069
Keedfik <65557454+Keedfik@users.noreply.github.com>
2020-05-20 19:13:11 +0300

    Update 3.1 (2).cpp

--- 3.1 (2).cpp
+++ 3.1 (2).cpp
@@ -1,5 +1,5 @@
-#include <iostream> // ďîäęëţ÷ŕĺě cin/cout
-#include <cmath> // ďîäęëţ÷ŕĺě sqrt
+#include <iostream> 
+#include <cmath> 
 
 using namespace std;
 
@@ -7,9 +7,8 @@ int main()
 {
     setlocale(LC_ALL, "Russian");
 
-    double a, b, c, d, x1, x2; // Îáú˙âë˙ĺě ďĺđĺěĺííűĺ ń ďëŕâŕţůĺé ňî÷ęîé.
-
-    cout << "a= "; // Ââîäčě čńőîäíűĺ äŕííűĺ
+    double a, b, c, d, x1, x2; 
+    cout << "a= "; 
     cin >> a;
     cout << "b= ";
     cin >> b;
@@ -20,21 +19,21 @@ int main()
         cout << "x1 = x2 = " << x1 << "\n";
     }
     else {
-        d = b * b - 4 * a * c; // Đŕńń÷čňűâŕĺě äčńęđčěčíŕíň
-        if (d > 0) // Óńëîâčĺ ďđč äčńęđčěčíŕíňĺ áîëüřĺ íóë˙
+        d = b * b - 4 * a * c; 
+        if (d > 0) 
         {
             x1 = ((-b) + sqrt(d)) / (2 * a);
             x2 = ((-b) - sqrt(d)) / (2 * a);
             cout << "x1 = " << x1 << "\n";
             cout << "x2 = " << x2 << "\n";
         }
-        if (d == 0) // Óńëîâčĺ äë˙ äčńęđčěčíŕíňŕ đŕâíîăî íóëţ
+        if (d == 0) 
         {
             x1 = -(b / (2 * a));
             cout << "x1 = x2 = " << x1 << "\n";
         }
-        if (d < 0) // Óńëîâčĺ ďđč äčńęđčěčíŕíňĺ ěĺíüřĺ íóë˙
-            cout << "D < 0, Äĺéńňâčňĺëüíűő ęîđíĺé óđŕâíĺíč˙ íĺ ńóůĺńňâóĺň";
+        if (d < 0) 
+            cout << "D < 0, no korni";
     }
     //system("pause");
 }
